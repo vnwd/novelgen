70950a8688aaa3691f72cb72a23e8ae422c05772
Michael Perez <michaelp193@gmail.com>
2014-03-30 02:21:16 -0400

    fixed formatting and added egg

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
+
   $('input[name=enter]').click(function() {
     var count = $('input[name=wordCount]').val();
     var responce = $("#responce");
-    responce.html('');
     var entry = RandWord(count);
+
+    responce.html('');
     $('#responce').html(entry);
     return false;
   });
+
   function RandWord (numOfWords) {
     var alpha = "abcdefghijklmnopqrstuvwxyz";
     var entry = '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp';
     var end = '.?!'
     var word = '';
+
+    if (!numOfWords) {
+      entry = "<h1>fuck you<h1>";
+    }
+
     for (var x = 0; x <= numOfWords; x += 1) {
       //appending word to entry
       entry += word;
